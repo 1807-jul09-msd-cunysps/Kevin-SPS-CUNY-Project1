5b64eff1fcd2288bf2cfc4f206e63b403e55bff8
tangkevin <tangkevin88@gmail.com>
2018-07-16 17:01:40 -0400

    Class definitions created

--- PhoneSolution/PhoneLibrary/Person.cs
+++ PhoneSolution/PhoneLibrary/Person.cs
@@ -9,6 +9,32 @@ namespace PhoneLibrary
     public class Person
     {
         public long Pid { get; set; }
-        public string  FirstName { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime DoB {
+            public DateTime DoB(int year, int month, int day);
+             {get ; set; }
+        }
+
+    }
+    public class Address
+    {
+        public long Pid { get; set; }
+        public string StreetName { get; set; }
+        public string HouseNum   {get; set;}
+        public string City   {get; set;}
+        public string State   {get; set;}
+        public int Zipcode { get; set; }
+        public Enum Country { get; set; }
+
+    }
+
+    public class Phone
+    {
+        public long Pid             {get;set;}
+        public int CountryCode      {get;set;}
+        public int AreaCode         {get;set;}
+        public int Number        { get; set; }
+
     }
 }
