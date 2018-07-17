9dc11cfcc9c6558ebf18ff529923a49c39af3320
tangkevin <tangkevin88@gmail.com>
2018-07-17 15:07:48 -0400

    PhoneDirectory.Delete() and Add() implemented

---
+++ PhoneSolution/PhoneLibrary/PhoneDirectory.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhoneLibrary
+{
+    class PhoneDirectory
+    {
+        List<Person> Directory;
+        PhoneDirectory()
+        {
+           Directory  = new List<Person>();
+        }
+        public void Add(Person p)
+        {
+            Directory.Add(p);
+        }
+        public void Delete(Person p)
+        {
+            foreach(var person in Directory)
+            {
+                if (p.Pid.Equals(person))
+                {
+                    Directory.Remove(person);
+                }
+            }
+        }
+
+    }
+}
