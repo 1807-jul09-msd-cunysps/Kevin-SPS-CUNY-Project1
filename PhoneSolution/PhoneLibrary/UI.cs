3afdc5f10f6471f0fa63a759e1247c706e09d9f9
tangkevin <tangkevin88@gmail.com>
2018-07-23 00:02:18 -0400

    Further refactoring and UI class clean up

--- PhoneSolution/PhoneLibrary/UI.cs
+++ PhoneSolution/PhoneLibrary/UI.cs
@@ -17,107 +17,137 @@ namespace PhoneLibrary
     }
 
     public class UI : IUserInterface(){
-
-        public List<Person> Contacts { get; set; }
-        #region UIDisplay()
-        public void UIDisplay()
+        //constructor//UIDisplay()
+        #region 
+        static List<string> sl;
+        public UI()
         {
-
-            string readonly s1 = "1. Read Contacts";
-            string readonly s2 = "2. Add Contact";
-            string readonly s3 = "3. Delete Contact";
-            string readonly s4 = "4. Edit Contact";
-            string readonly s5 = "5. Search Contacts";
-            private static readonly string[] _ui = { s1, s2, s3, s4, s5 };
-            foreach (var s in _ui){ 
+            sl.Add("Welcome to PhoneApp");
+            sl.Add(""Please enter the key corresponding to desired action: "");
+            sl.Add("1. Read Contacts");
+            sl.Add("2. Add Contact");
+            sl.Add("3. Delete Contact");
+            sl.Add("4. Edit Contact");
+            sl.Add("5. Search Contacts");
+        }
+        public void PrintS()
+        {
+            foreach(string s in sl)
+            {
                 Console.WriteLine(s);
             }
-    #endregion
-        int UIRead(){
         }
+          
+        
+    #endregion
+        public List<Person> Contacts { get; set; }
+        
+        #region 
 
-}
-
-        public void UISwitch(int input)
-        {
-            switch (input)
-            {
-                default:
-                //Switchboard()
-                case 0:
-                    break;
-                case 1:
-                    ReadContacts();
-                case 2:
-                //AddContact()
-                case 3:
-                //DeleteContact()
-                case 4:
-                //EditContact()
-                case 5:
-                //SearchContact()
-               
+        public void UIDisplay() {
+            foreach (var s in this._s) {
+                Console.WriteLine(s);
 
             }
-        }
+            #endregion
+            int UIRead()
+            {
+                int number;
+                string input = Console.ReadLine();
+                if (!Int32.TryParse(input, out number))
+                {
+                    Console.WriteLine('Please enter an integer');
+                    UIRead();
+                }
+                else
+                {
+                    return number;
+                }
+                return 0;
+            }
+            void UISwitch(int input)
+            {
+                switch (input)
+                {
 
-        public void ReadContacts()
-        {
+                    case 0:
+                        break;
+                    case 1:
+                        ReadContacts();
+                        break;
+                    case 2:
+                        Person p = new Person(Person.GetPersonInfo());
+                        AddContact();
+                        break;
+                    case 3:
+                        DeleteContact();
+                        break;
+
+                    case 4:
+                        EditContact();
+                        break;
+                    case 5:
+                        //SearchContact()
+                        break;
+                    default:
+                        //Switchboard()
+                        break;
+
+                }
+            }
+            #region
+            public void ReadContacts()
+            {
             //Initialize Property of Collection of Persons
            private List<Person> pList;
 
-            public List<Person> PList { get => pList; set => pList = value; }
+        //IO stream to obtain JSON
 
-            //IO stream to obtain JSON
+        //Deserialize JSON
 
-            //Deserialize JSON
+        /*string filepath = "C:\Users\Kevin\Source\Repos\Kevin-SPS-CUNY-Project0.contacts.json";
+            FileStream stream1 = new FileStream(filepath, FileMode.Open; FileAccess.Read);
+        */
+        //Set contacts to the newly read contact list
 
-            /*string filepath = "C:\Users\Kevin\Source\Repos\Kevin-SPS-CUNY-Project0.contacts.json";
-                FileStream stream1 = new FileStream(filepath, FileMode.Open; FileAccess.Read);
-            */
-            //Set contacts to the newly read contact list
-            
-                contacts = PList;      
-         }
-        //Passes in a Person object and adds it to the Collection of Persons
-        public static void AddContact(Person person, ref List<Person> listPersons)
+        contacts = PList;      
+         
+    #endregion
+
+    //Passes in a Person object and adds it to the Collection of Persons
+    public void AddContact(Person person, ref List<Person> listPersons)
         {
-            
+
             listPersons.Add(person);
-            Console.WriteLine($'New Contact \' '+ person.FirstName + ' has beend added to Contacts.');
+            Console.WriteLine($"New Contact {person.FirstName} has been added to Contacts.");
         }
 
-        public static string DeleteContact()
+        public void DeleteContact(Person person, ref List<Person> listPersons)
+        {
+            listPersons.Remove(person);
+            Console.WriteLine($"New Contact {person.FirstName} has been removed Contacts.");
+        }
+        public void EditContact(Person person, ref List<Person> listPersons)
         {
 
         }
-        public static string EditContact()
+        public Person SearchContact(in List<Person> Directory)
         {
+            Console.WriteLine('Enter first name of person');
+            string fName = Console.ReadLine();
+            //search collections
 
-        }
-        public static string SearchContact()
-        {
-            var persons = p.Get();
-            //LINQ-Language Integrate Query
-            //Query Syntax
-            /*var query = from p1 in persons
-                        where p1.firstName.StartsWith("T")
-                        select p1;*/
-            var query = from p1 in persons
-                        where p1.address.houseNum.Equals("121")
-                        select p1;
-
+            foreach (Person p in Directory)
+            {
+                if (p.FirstName.Equals(fName))
+                {
+                    return p;
+                }
+            }
+            return null;
         }
 
-        public static Person AddPerson()
-        {
-            Person p = new Person;
-            Console.WriteLine("Please enter " + string s);
-            Console.ReadLine(string input);
-
-            return  p;
     }
-
+    
 }
-
 
