d6dd74dc280da0526d38de6974d9f71389265c53
tangkevin <tangkevin88@gmail.com>
2018-07-31 15:31:26 -0400

    Initial commit

---
+++ PhoneSolution/UI Test/UnitTest1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PhoneAppLibrary;
+
+namespace UI_Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        Person p = new Person(Person.PopulatePersonListString());
+        Person q = new Person(Person.PopulatePersonListString());
+        q.FirstName = q.FirstName.Concat("er");
+        [TestMethod]
+        public void TestSearchContactById(int id)
+        {
+            //arrange
+
+            //act
+
+            //assert
+        }
+    }
+}
---
+++ PhoneSolution/WebPhoneContact/Scripts/CustomJS/EventHandler.js
@@ -0,0 +1,14 @@
+﻿$("#aboutme").attr("href", "aboutme.html");
+
+function validate() {
+
+        if ($("#age").val() < 15 || $("#age").val() > 120) {
+            $("#age").attr(border: 2px solid red);
+        }
+        else {
+            $("#age").attr("");
+        }
+    }()
+
+
+}
