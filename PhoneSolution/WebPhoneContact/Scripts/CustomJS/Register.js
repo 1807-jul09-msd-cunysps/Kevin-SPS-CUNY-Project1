52d8d3747a272a5cee4dbd831553cc1a4b19bf2a
tangkevin <tangkevin88@gmail.com>
2018-08-03 17:47:39 -0400

    RevalidateAge() update

--- PhoneSolution/WebPhoneContact/Scripts/CustomJS/Register.js
+++ PhoneSolution/WebPhoneContact/Scripts/CustomJS/Register.js
@@ -45,11 +45,8 @@ function checkAdd() {
 function RevalidateAge() {
     debugger;
     var dob = $("#DoB").val();
-    alert(dob);
     var present = new Date();
-    alert(present);
     var age = Date.DateDiff("yyyy", dob, present)
-    alert(age);
     var dob_element = $("#DoB");
   
     if (age < 15 || age > 110) {
@@ -58,28 +55,39 @@ function RevalidateAge() {
         dob_element.addClass("is-invalid");
         var dobHelp = $("#dobHelp");
         var content = document.createTextNode("Age not within 15:110 years of age");
-        dobHelp.appendChild(content);
+        alert(typeof (dobHelp));
+        dobHelp.innerHTML = "Age not within 15: 110 years of age";
     }
     else {
         dob_element.addClass("is-valid");
         dob_element.removeClass("is-invalid");
-        
+        dobHelp.innerHTML = '';
 
 
     }
 
 }
-
-function checkContactForm(e) {
-    //Need to validate form before submission
+function checkZipcode() {
     debugger;
-    var mail = document.getElementById("mail").value;
-    var name = document.getElementById("name").value;
-    var msg = document.getElementById("msg").value;
-    var error = document.getElementById("error").value;
+    var zipcode = document.getElementById("zipcode").value;
+    var clientKey = "G1KuYkWJ8voqjnOO5RF9xMYvsaF1zlMWR0mR2K9IURb0Vc8vYn7T1qFcga8ZSkUx";
+    var url = "https://www.zipcodeapi.com/rest/" + clientKey + "/info.json/" + zipcode + "/radians";
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', url);
+    xhr.onreadystatechange = function () {    //Call a function when the state changes.
+        if (this.readyState == XMLHttpRequest.DONE && this.status == 200) {
+            var result = xhr.responseText;
+            if (result !== null || result !== "" || result !== undefined) {
+                if (result.charAt(0) === '"' && result.charAt(result.length - 1) === '"') {
+                    result = result.substr(1, result.length - 2);
+                }
+                //debugger;
+                locationPopulate(result);
+            }
 
+        }
+    }
+    xhr.send();
 }
-
-
 //e.preventDefault:     Prevents the submission from being submission
 //$("#aboutme").attr("href", "aboutme.html");
